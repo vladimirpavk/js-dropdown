5bfa9ff12359046c7ce5290ca3f2fb7d76f6c324
Vladimir Pavković <vladimirpavk@gmail.com>
2021-01-27 14:23:42 +0100

    +

--- src/script/performance.js
+++ src/script/performance.js
@@ -1,5 +1,15 @@
 const { performance } = require('perf_hooks');
-const { heapPerm, combAll, heapPermIterative, combAllStrict } = require('./utils.common-js.js');
+console.log(require('./utils.common-js.js'));
+const 
+{ 
+    heapPerm,
+    combAll,
+    heapPermIterative,
+    combAllStrict,
+    fib,
+    makeRangeIterator,
+    makeRangeIterator2
+} = require('./utils.common-js.js');
 
 let nizOrigin  = ['a', 'b', 'c', 'd', 'e'];
 let niz = [...nizOrigin];
@@ -24,3 +34,25 @@ startTime = performance.now();
 let BinaryCombinationsStrict = combAllStrict(niz, 'f', 3, true);
 endTime = performance.now();
 console.log(endTime - startTime, 'num_of_permutations :', BinaryCombinationsStrict.length);
+
+for(let n of fib){
+    if(n > 1000) break;
+    console.log(n);
+}
+
+const it = makeRangeIterator(0, 100, 5);
+let result = it.next();
+while(!result.done){   
+    console.log(result.value);
+    result = it.next();
+}
+console.log('Num of iterations ', result.value);
+
+const rit = makeRangeIterator2(0, 100, 3);
+
+//for(let x of rit) console.log(x);
+rit.forEach(element => {
+    console.log(element);
+});
+
+console.log('kraj');
--- src/script/utils.common-js.js
+++ src/script/utils.common-js.js
@@ -159,9 +159,64 @@ const combAllStrict = (anyArray, direction, length, strict)=>{
     return resultArray;
 }
 
+let fibonacci = {
+    [Symbol.iterator]() {
+        let pre = 0, cur = 1;
+        return{
+            next(){
+                pre = cur;
+                cur = pre + cur;
+                return {
+                    done: false,
+                    value: cur
+                }
+            }
+        }
+    }
+}
+
+const makeRangeIterator = (start = 0, end = Infinity, step = 1) =>{
+    let nextIndex = start;
+    let iterations = 0;
+
+    const rangeIterator = {
+        next: ()=>{
+            if(nextIndex <= end){
+                let result = {
+                    value: nextIndex,
+                    done: false
+                }
+               nextIndex+=step; 
+               iterations++; 
+               return result;     
+            }
+            return{
+                value: iterations,
+                done: true
+            }
+        }
+    }
+
+    return rangeIterator;
+}
+
+function* makeRangeIterator2(start = 0, end = Infinity, step = 1){
+    iterationCounter = 0;
+
+    for(let i = start; i < end; i+=step){
+        iterationCounter++;
+        yield i;
+    }
+
+    return iterationCounter;
+}
+
 module.exports = {
     'heapPerm' : heapPerm,
     'heapPermIterative' : heapPermIterative,
     'combAll' : combAll,
-    'combAllStrict' : combAllStrict
+    'combAllStrict' : combAllStrict,
+    'fib' : fibonacci,
+    'makeRangeIterator' : makeRangeIterator,
+    'makeRangeIterator2' : makeRangeIterator2
 }
