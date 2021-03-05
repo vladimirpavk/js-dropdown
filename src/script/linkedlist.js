e09d388440074ed581f479df82084694852c28ba
Vladimir Pavković <vladimirpavk@gmail.com>
2021-03-05 12:12:26 +0100

    +

--- src/script/linkedlist.js
+++ src/script/linkedlist.js
@@ -1,3 +1,5 @@
+const { node } = require("webpack");
+
 //linkedlist class
 class Node{
     constructor(value, nextPointer){
@@ -10,9 +12,10 @@ class LinkedList{
 
     constructor(){
         this.startPointer = null;
+        this.length = 0;
     }
 
-    addNodeEnd(value){
+    addNode(value){
         let newNode = new Node(value);
         
         if(this.startPointer === null){
@@ -24,14 +27,40 @@ class LinkedList{
             while(pointer.nextPointer!==null) pointer=pointer.nextPointer;
             pointer.nextPointer = newNode;     
         }    
+
+        this.length = this.length + 1;
+    }
+
+    addNodeAtIndex(value, index){
+        let pointer = this.startPointer;
+        let newNode = new Node(value, null);
+        for(let i = 1; i<index; i++){
+            if(pointer.nextPointer === null) return false;
+            pointer = pointer.nextPointer;
+        }
+        //u poziciji sam za promenu linkova        
+        newNode.nextPointer = pointer.nextPointer;
+        pointer.nextPointer = newNode;
     }
 
     addArray(arrayList){
-        this.addNodeEnd(...arrayList);
+       arrayList.forEach(element => {
+           this.addNode(element);
+       });
     }
 
-    printList(node){
-        while(pointer.nextPointer!==null) console.log(node.value);
+    printList(){
+        let pointer = this.startPointer;
+        while(pointer!==null){
+            console.log(pointer.value);
+            pointer = pointer.nextPointer;
+        }
+    }
+
+    printListReverse(pointer = this.startPointer){
+        if(pointer===null) return;
+        this.printListReverse(pointer.nextPointer);
+        console.log(pointer.value);
     }
 }
 
--- src/script/mainList.js
+++ src/script/mainList.js
@@ -5,5 +5,10 @@ const {
 
 let ll = new LinkedList();
 ll.addArray([5,2,3,4,5,6]);
+ll.printList();
 
-console.log('kraj');
+console.log('recursion');
+ll.printListReverse();
+
+console.log(ll.addNodeAtIndex(5, 2));
+ll.printList();
